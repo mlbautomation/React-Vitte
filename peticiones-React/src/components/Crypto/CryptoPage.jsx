fa77e88b8f74b2086c0bef245eb28a0d2ff4281d
mlbautomation <mlbautomation@gmail.com>
2024-08-28 16:33:25 -0500

    hook custom

--- peticiones-React/src/components/Crypto/CryptoPage.jsx
+++ peticiones-React/src/components/Crypto/CryptoPage.jsx
@@ -1,46 +1,25 @@
 //See! useParams is from "react-router-dom", not from from "react-router"
-import axios from "axios";
-import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { parseFloatNumber } from "../helpers/numbers";
+import { parseFloatNumber } from "../../helpers/numbers";
 import "./CryptoPage.css";
+import usePetition from "../../hook/usePettion";
 
 const CryptoPage = () => {
-  const API_URL = import.meta.env.VITE_API_URL;
   const params = useParams();
 
-  const [crypto, setCrypto] = useState({});
-  useEffect(() => {
-    axios
-      .get(`${API_URL}assets/${params.id}`)
-      .then((data) => {
-        setCrypto(data.data.data);
-      })
-      .catch((e) => {
-        console.error(e);
-      });
-  }, []);
-
-  const [history, setHistory] = useState([]);
-  useEffect(() => {
-    axios
-      .get(`${API_URL}assets/${params.id}/history?interval=d1`)
-      .then((data) => {
-        setHistory(data.data.data);
-      })
-      .catch((e) => {
-        console.error(e);
-      });
-  }, []);
+  const crypto = usePetition(`assets/${params.id}`);
+  const history = usePetition(`assets/${params.id}`);
 
   return (
     <div className="crypto-page-container">
       {/* Fundamental details of the Cryptocurrency */}
-      <div className="info">
-        <span>Ranking: {crypto.rank}</span>
-        <h1>{crypto.name}</h1>
-        <span className="symbol">{crypto.symbol}</span>
-      </div>
+      {
+        <div className="info">
+          crypto && (<span>Ranking: {crypto.rank}</span>
+          <h1>{crypto.name}</h1>
+          <span className="symbol">{crypto.symbol}</span>)
+        </div>
+      }
       {/* Basic details of the Cryptocurrency */}
       <div className="details">
         <ul>
@@ -72,22 +51,24 @@ const CryptoPage = () => {
       </div>
       {/* History informormation table */}
       <div className="history">
-        <table>
-          <thead>
-            <tr>
-              <th>Date</th>
-              <th>Price</th>
-            </tr>
-          </thead>
-          <tbody>
-            {history.reverse().map(({ date, priceUsd, time }) => (
-              <tr key={time}>
-                <td className="label">{new Date(date).toDateString()}</td>
-                <td className="price">{parseFloatNumber(priceUsd, 3)}</td>
+        {history && (
+          <table>
+            <thead>
+              <tr>
+                <th>Date</th>
+                <th>Price</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {history.reverse().map(({ date, priceUsd, time }) => (
+                <tr key={time}>
+                  <td className="label">{new Date(date).toDateString()}</td>
+                  <td className="price">{parseFloatNumber(priceUsd, 3)}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
       </div>
     </div>
   );
