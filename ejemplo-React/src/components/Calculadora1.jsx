b7d4af439c48cefe72c4ca5c090b3d443ce2218b
Marlon Ly <111965772+mlbautomation@users.noreply.github.com>
2024-08-30 18:04:15 -0500

    ejemplo-React improved

--- ejemplo-React/src/components/Calculadora1.jsx
+++ ejemplo-React/src/components/Calculadora1.jsx
@@ -26,75 +26,88 @@ un bind, de lo contrario no lo reconoce*/
 4. Se desmonta */
 
 /* Los componentes que tiene estado se llaman: Statefull
-caso contrario se llama: Stateless */
+caso contrario se llama: Stateless
+
+Antes en React solo los componentes del tipo clase
+podían tener estado, pero ahora las funciones pueden.
+*/
 
 class Calculadora1 extends Component {
-    //Primer paso del ciclo de vida
-    constructor(props) {
-        super(props)
-        this.state = {
-            numero1: 0,
-            numero2: 0
-        }
+  //Primer paso del ciclo de vida
+  constructor(props) {
+    /* 
+        Herencia: envía la información 
+        a la clase padre Component, esto
+        es de JS.
+    */
+    super(props);
+    this.state = {
+      numero1: 0,
+      numero2: 0,
+    };
 
-        this.setNumber1 = this.setNumber1.bind(this)
-    }
+    /* En el constructor es necesario hacer bind
+    para que un método sea reconocido y utilizado 
+    en mi clase, POO de JS */
+    this.setNumber1 = this.setNumber1.bind(this);
+    this.setNumber2 = this.setNumber2.bind(this);
+  }
 
-    setNumber1(e) {
-        return (
-            this.setState({
-                ...this.state,
-                numero1: e.target.value
-            })
-        )
-    }
-    //Segundo paso del ciclo de vida
-    render() {
-        return (
-            <>
-                <span>Hola mundo desde una clase</span>
-                <h2>{this.props.nombre}</h2>
-                <form>
-                    <input
-                        type="number"
-                        name="numero1"
-                        value={this.state.numero1}
-                        onChange={this.setNumber1}
-                    />
-                    <input
-                        type="number"
-                        name="numero2"
-                        value={this.state.numero2}
-                        onChange={e => {
-                            this.setState({
-                                ...this.state,
-                                numero2: e.target.value
-                            })
-                        }}
-                    />
-                    <input type="submit" name="enviar" value="Calcular" />
-                </form>
-                <span>Resultado: {Number(this.state.numero1) + Number(this.state.numero2)} </span>
-            </>
-        )
-    }
+  //De forma implícita los metodos reciben el evento
+  setNumber1(e) {
+    return this.setState({
+      ...this.state,
+      numero1: e.target.value,
+    });
+  }
+  setNumber2(e) {
+    return this.setState({
+      ...this.state,
+      numero2: e.target.value,
+    });
+  }
+  //Segundo paso del ciclo de vida
+  render() {
+    return (
+      <>
+        <span>Hola mundo desde una clase</span>
+        <h2>{this.props.nombre}</h2>
+        <form>
+          <input
+            type="number"
+            name="numero1"
+            value={this.state.numero1}
+            onChange={this.setNumber1}
+          />
+          <input
+            type="number"
+            name="numero2"
+            value={this.state.numero2}
+            onChange={this.setNumber2}
+          />
+          <input type="submit" name="enviar" value="Calcular" />
+        </form>
+        <span>
+          Resultado: {Number(this.state.numero1) + Number(this.state.numero2)}{" "}
+        </span>
+      </>
+    );
+  }
 
-    /* Tercer paso del ciclo de vida: Una vez que
+  /* Tercer paso del ciclo de vida: Una vez que
     el componente hay sido montado en el DOM:
     Debuging de JS
     ¿Qué hace que un componente se renderice nuevamente?
     1. Actualización de estados
     2. Que otro componente le haya pasado una prop diferente*/
-    componentDidMount() {
-        console.log("El componente se esta renderizando nuevamente, incorrecto")
-    }
+  componentDidMount() {
+    console.log("El componente se esta renderizando nuevamente, incorrecto");
+  }
 
-    /* Cuarto paso del ciclo de vida: Va a dejar
+  /* Cuarto paso del ciclo de vida: Va a dejar
     de existir en el DOM:
     Terminar Lissening, Interval, Timeout de JS*/
-    componentWillUnmount() {
-
-    }
+  componentWillUnmount() {}
 }
 
-export default Calculadora1
+export default Calculadora1;
